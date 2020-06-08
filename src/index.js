e4844d7faec322219742aab95750ab11c8e628b2
Mhairi Hislop <mhairihislop@Mhairis-MacBook-Pro.local>
2020-06-08 15:54:55 +0100

    adding functionality to buttons for player1, 2 and reset

--- src/index.js
+++ src/index.js
@@ -15,21 +15,42 @@ const initialState = {
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "INCREMENT": return { ...state, value: state.value + 1 };
-    case "RESET": return { ...state, value: 0 }
+    case "PLAYER_1": return { ...state, player1: state.player1 + 1 };
+    case "PLAYER_2": return { ...state, player2: state.player2 + 1 };
+    case "RESET": return initialState;
     default: return state;
   }
 };
 
-const store = createStore(reducer, initialState);
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+
+const render = () => {
+  let state = store.getState();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        player1={state.player1}
+        player2={state.player2}
+        handlePlayer1={() => store.dispatch({ type: "PLAYER_1" })}
+        handlePlayer2={() => store.dispatch({ type: "PLAYER_2" })}
+        handleReset={() => store.dispatch({ type: "RESET" })}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+};
+
+store.subscribe(render);
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
